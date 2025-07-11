fb71a445056bbc3ed3a5f536838c11c1f4c66ca0
Kevin <139545370+Kany2t@users.noreply.github.com>
2025-07-11 20:20:49 +0800

    init

--- function/main.go
+++ function/main.go
@@ -15,19 +15,22 @@ func main() {
 	//调用函数
 	fmt.Println(_case.Sum(1, 2))
 	//函数赋值给变量
-	f1 := _case.Sum
+	var f1 _case.SumFunc = _case.Sum
+	//f1 := _case.Sum
 	fmt.Println(f1(1, 2))
 
 	//	将函数作为输入输出中间件
-	f2 := _case.LogMiddleware(_case.Sum)
+	var f2 _case.SumFunc = _case.LogMiddleware(_case.Sum)
+	//f2 := _case.LogMiddleware(_case.Sum)
 	//再次申请中间件
 	fmt.Println(f2(1, 2))
-	f3 := _case.SumFunc(f1)
+	f3 := _case.SumFunc(f2)
 	fmt.Println(f3.Accumulation(1, 2, 3, 4))
 	fmt.Println(f2.Accumulation(1, 2, 3, 4, 5))
 
 	fmt.Println(_case.Fib(10))
 
+	//闭包的陷阱
 	_case.ClosureTrap()
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
 	defer cancel()
